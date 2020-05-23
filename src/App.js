421bf02baa5240371d8262fca77fe713af255162
Kevin <Atnnetworkk@gmail.com>
2020-05-23 21:37:23 +0200

    Add header & style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/css/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="header">
+        <a href="/">
+          <img
+            srcSet="./img/logo-purple.svg"
+            src="./img/logo-purple.png"
+            alt="logo"
+            className="header__logo"
+            height="34"
+            width="34"
+          />
         </a>
+        <button className="header__menu">
+          <img
+            srcSet="./img/icons/menu-purple.svg"
+            src="./img/icons/menu-purple.png"
+            alt=""
+            className="header__menu-icon"
+          />
+        </button>
       </header>
     </div>
   );
