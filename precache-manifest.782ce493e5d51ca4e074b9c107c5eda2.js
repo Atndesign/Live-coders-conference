b31fe529b59235ff3652284e4ddfd51820f424f8
Kevin <Atnnetworkk@gmail.com>
2020-05-24 17:12:41 +0200

    Updates

---
+++ precache-manifest.782ce493e5d51ca4e074b9c107c5eda2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acb14958864a259eccbab196558b893",
+    "url": "/Live-coders-conference/index.html"
+  },
+  {
+    "revision": "41786fca9366f666f9be",
+    "url": "/Live-coders-conference/static/css/main.dc9e8c4b.chunk.css"
+  },
+  {
+    "revision": "2e36bd5d685cae3e644b",
+    "url": "/Live-coders-conference/static/js/2.921a92ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Live-coders-conference/static/js/2.921a92ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41786fca9366f666f9be",
+    "url": "/Live-coders-conference/static/js/main.ce11a7b8.chunk.js"
+  },
+  {
+    "revision": "7d4a2793dd1b31c6c715",
+    "url": "/Live-coders-conference/static/js/runtime-main.09467ad6.js"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/Live-coders-conference/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "f3a02e2578bee50e620e515912278bc9",
+    "url": "/Live-coders-conference/static/media/Roboto-Bold.f3a02e25.woff2"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/Live-coders-conference/static/media/Roboto-Regular.94dac78e.woff"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/Live-coders-conference/static/media/Roboto-Regular.9feb0110.woff2"
+  },
+  {
+    "revision": "67f4f8524b13e7b5bf7d28e3e32f1758",
+    "url": "/Live-coders-conference/static/media/waveDesktop.67f4f852.svg"
+  }
+]);
